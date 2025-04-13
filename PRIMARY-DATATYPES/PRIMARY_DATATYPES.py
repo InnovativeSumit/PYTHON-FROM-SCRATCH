666da8f40a092c954cfe884462fe2b479a5f0eb2
InnovativeSumit <pal2024santosh@gmail.com>
2025-04-13 19:49:25 +0530

    PRIMARY-DATATYPES
    Primary Data Types in Python:
    int – Stores whole numbers.
    str – Stores a sequence of characters (strings).
    bool – Stores Boolean values: True or False.
    None – Represents the absence of a value.
    float – Stores decimal numbers.

---
+++ PRIMARY-DATATYPES/PRIMARY_DATATYPES.py
@@ -0,0 +1,14 @@
+# in python there are 5 primary datatypes
+# integer , string , float , boolean , none
+a=2
+b="sumit"
+c=3.45
+d=False
+e=True
+f=None
+print("vriable of a:",a,"is a type of INTEGER datatype")
+print("vriable of b:",b,"is a type of STRING datatype")
+print("vriable of c:",c,"is a type of DOUBLE datatype")
+print("vriable of d:",d,"is a type of BOOLEAN datatype")
+print("vriable of e:",e,"is a type of BOOLEAN datatype")
+print("vriable of f:",f,"is a type of NONE datatype")
---
+++ PRIMARY-DATATYPES/PRINT_DATATYPES_TAKEN_FROM_USERS.py
@@ -0,0 +1,15 @@
+a=int(input("Enter the value of a:"))
+print("variable of a",a,"is a",type(a),"datatype")
+
+b=float(input("Enter the value of a:"))
+print("variable of b",b,"is a",type(b),"datatype")
+
+c=bool(input("Enter the value of a:"))
+print("variable of c",c,"is a",type(c),"datatype")
+
+d=str(input("Enter the value of a:"))
+print("variable of d",d,"is a",type(d),"datatype")
+
+e=None(input("Enter the value of a:")) 
+print("variable of e",e,"is a",type(e),"datatype") #TypeError: 'NoneType' object is not callable
+
