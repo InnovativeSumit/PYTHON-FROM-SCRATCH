e9e4174569c9e93b651002b81051ea0930a378ed
InnovativeSumit <pal2024santosh@gmail.com>
2025-04-15 12:31:50 +0530

    ESCAPE_SEQUENTIAL-CHARACTERS
    \n → New line
    \t → Horizontal tab
    \\ → Backslash
    \" → Double quote
    \' → Single quote
    \a → Alert (bell sound)
    \b → Backspace
    \r → Carriage return
    \f → Form feed
    \v → Vertical tab

---
+++ ESCAPE_SEQUENCTIAL_CHARACTERS/ESCAPE_SEQUENCTIAL_CHARACTERS.py
@@ -0,0 +1,34 @@
+# Escape Sequences in Python
+
+# \n - New line
+print("This is the first line.\nThis is the second line.")
+
+# \t - Horizontal tab
+print("Name\tAge\tCity")
+
+# \\ - Backslash
+print("This is a backslash: \\")
+
+# \" - Double quote
+print("He said, \"Python is awesome!\"")
+
+# \' - Single quote
+print('It\'s a beautiful day.')
+
+# \a - Alert / Bell sound (may or may not work depending on system)
+print("This may produce a beep sound: \a")
+
+# \b - Backspace (removes previous character)
+print("Helloo\b!")
+
+# \r - Carriage return (returns cursor to beginning of line)
+print("Carriage return:\rOverwritten")
+
+# \f - Form feed (mostly used in printers, rarely seen on screen)
+print("Form feed\fNew section?")
+
+# \v - Vertical tab (not commonly used)
+print("Vertical tab\vCheck spacing")
+
+# \? - Not needed in Python like in C/C++, but can still be written
+print("Is this a question\? Yes it is.")
