5bb062dfb7834d20eff57c9bdceebcf05516b9a8
InnovativeSumit <pal2024santosh@gmail.com>
2025-04-16 13:07:28 +0530

    ARITHMATIC-OPERATORS
    Arithmetic operators perform basic math operations:
    + (Addition)
    - (Subtraction)
    * (Multiplication)
    / (Division)
    % (Modulus: remainder)

---
+++ OPERATORS/ARITHMATIC_OPERATORS.py
@@ -0,0 +1,20 @@
+#Mkae by Sumit
+a=20
+b=2
+print("THE ADDITION IS :",a+b)
+print("THE SUBTRACTION IS",a-b)
+print("THE MULTIPLICATION IS",a*b)
+print("THE DIVISION IS",a/b)
+print("THE REMIANDER IS",a%b,"\n\n")  # %-> MODULO OPERATORS
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+print("NOW WE STORE THE OPERSTION IN OTHER VARIABLE")
+print("============================================")
+print("THE ADDITION IS :",c)
+print("THE SUBTRACTION IS",d)
+print("THE MULTIPLICATION IS",e)
+print("THE DIVISION IS",f)
+print("THE REMIANDER IS",g)
