b794e69a6055acf752fdfa11c2c4172ccab5a93c
InnovativeSumit <pal2024santosh@gmail.com>
2025-04-18 10:12:39 +0530

    Delete RELATIONAL_OPERATORS directory

--- RELATIONAL_OPERATORS/RELATIONAL_OPERATORS.py
+++
@@ -1,8 +0,0 @@
-a=12
-b=20
-print(a,"==",b,"is",(a==b)) # FALSE
-print(a,"!=",b,"is",(a!=b)) # TRUE
-print(a,">",b,"is",(a>b)) # FALSE
-print(a,"<",b,"is",(a<b)) # TRUE
-print(a,">=",b,"is",(a>=b)) # FALSE
-print(a,"<=",b,"is",(a<=b)) # TRUE
--- RELATIONAL_OPERATORS/RELATIONAL_OPERATORS_DATA_TAKEN_FROM_USER.py
+++
@@ -1,8 +0,0 @@
-a=int(input("ENTER YOUR 1ST NUMBER: "))
-b=int(input("ENTER YOUR 2ND NUMBER: "))
-print(a,"==",b,"is",(a==b)) # FALSE
-print(a,"!=",b,"is",(a!=b)) # TRUE
-print(a,">",b,"is",(a>b)) # FALSE
-print(a,"<",b,"is",(a<b)) # TRUE
-print(a,">=",b,"is",(a>=b)) # FALSE
-print(a,"<=",b,"is",(a<=b)) # TRUE
