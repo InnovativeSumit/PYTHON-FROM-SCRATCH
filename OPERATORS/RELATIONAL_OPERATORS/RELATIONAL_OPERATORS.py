e29ef514aa7360815c7b0ed1fcd5e27f85af7b54
InnovativeSumit <pal2024santosh@gmail.com>
2025-04-18 10:14:48 +0530

    RELATIONAL-OPERATORS
    Key Relational Operators
    Equal to (==)
    Checks if two values are the same.
    Example: 5 == 5 → True
    
    Not equal to (!= or <>)
    Checks if two values are different.
    Example: 5 != 3 → True
    
    Greater than (>)
    Checks if the left value is larger than the right.
    Example: 10 > 7 → True
    
    Less than (<)
    Checks if the left value is smaller than the right.
    Example: 4 < 6 → True
    
    Greater than or equal to (>=)
    Checks if the left value is larger or equal to the right.
    Example: 8 >= 8 → True
    
    Less than or equal to (<=)
    Checks if the left value is smaller or equal to the right.
    Example: 5 <= 5 → True

---
+++ OPERATORS/RELATIONAL_OPERATORS/RELATIONAL_OPERATORS.py
@@ -0,0 +1,8 @@
+a=12
+b=20
+print(a,"==",b,"is",(a==b)) # FALSE
+print(a,"!=",b,"is",(a!=b)) # TRUE
+print(a,">",b,"is",(a>b)) # FALSE
+print(a,"<",b,"is",(a<b)) # TRUE
+print(a,">=",b,"is",(a>=b)) # FALSE
+print(a,"<=",b,"is",(a<=b)) # TRUE
