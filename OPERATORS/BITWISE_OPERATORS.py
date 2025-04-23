f7016b6af8aeb2adaa492553a9eff57cde2ef363
InnovativeSumit <pal2024santosh@gmail.com>
2025-04-23 16:46:57 +0530

    BIITWISE-OPERATORS

---
+++ OPERATORS/BITWISE_OPERATORS.py
@@ -0,0 +1,26 @@
+a = 10  # 1010 in binary
+b = 4   # 0100 in binary
+
+print("a =", a)
+print("b =", b)
+
+# AND
+print("a & b =", a & b)
+
+# OR
+print("a | b =", a | b)
+
+# XOR
+print("a ^ b =", a ^ b)
+
+# NOT
+print("~a =", ~a)
+print("~b =", ~b)
+
+# Left Shift
+print("a << 1 =", a << 1)
+print("b << 2 =", b << 2)
+
+# Right Shift
+print("a >> 1 =", a >> 1)
+print("b >> 2 =", b >> 2)
